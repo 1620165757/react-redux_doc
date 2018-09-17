475fa24ee66e4ff307b823aa381b7a616fd90660
zhuxiaosong <1620165757@qq.com>
2018-09-17 23:34:55 +0800

    ..

--- src/index.js
+++ src/index.js
@@ -1,14 +1,48 @@
-import store from './store.js';
-import { addToCart }  from './actions/cart-actions';
+import { createStore } from "redux";
+import { combineReducers } from 'redux';
+
+const initialState = {
+    cart: [
+        {
+            product: 'bread 700g',
+            quantity: 2,
+            unitCost: 90
+        },
+        {
+            product: 'milk 500ml',
+            quantity: 1,
+            unitCost: 47
+        }
+    ]
+};
+
+const productsReducer = function(state=[], action) {
+    return state;
+};
+
+const ADD_TO_CART = 'ADD_TO_CART';
+
+const cartReducer = function(state=initialState, action) {
+    switch (action.type) {
+        case ADD_TO_CART: {
+            return {
+                ...state,
+                cart: [...state.cart, action.payload]
+            }
+        }
+
+        default:
+            return state;
+    }
+};
+
+const allReducers = {
+    products: productsReducer,
+    shoppingCart: cartReducer
+};
+
+const rootReducer = combineReducers(allReducers);
+
+let store = createStore(rootReducer);
 
 console.log("initial state: ", store.getState());
-
-let unSubscribe = store.subscribe(() =>
-    console.log(store.getState())
-);
-
-store.dispatch(addToCart('Coffee 500gm', 1, 250));
-store.dispatch(addToCart('Flour 1kg', 2, 110));
-store.dispatch(addToCart('Juice 2L', 1, 250));
-
-unSubscribe();
